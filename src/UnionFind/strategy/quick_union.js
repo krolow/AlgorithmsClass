a8fe433529e0ea1c892c1ce6364e2660f95ca398
Vinícius Krolow <krolow@gmail.com>
2014-07-01 21:29:32 -0300

    Adding quick union algorithm

---
+++ src/UnionFind/strategy/quick_union.js
@@ -0,0 +1,59 @@
+/**
+ * QuickUnion alogorithm
+ *
+ * @param {Int} numberOfItems The number of items
+ */
+var QuickUnion = function (numberOfItems) {
+	this.map = this.generateMap(numberOfItems);
+};
+
+/**
+* Generate a map array with the index
+*
+* @param {Int} numberOfObjects Number of items
+*/
+QuickUnion.prototype.generateMap = function (numberOfItems) {
+	var map = new Array(numberOfItems);
+	for (var i=0; i<numberOfItems; i++) {
+		map[i] = i;
+	}
+
+	return map;
+};
+
+/**
+* Union two points
+*
+* @param  {Int} p First Point
+* @param  {Int} q Second Point
+* @return {QuickFind} Quick find reference object
+*/
+QuickUnion.prototype.union = function (p, q) {
+	this.map[this.root(p)] = this.root(q);
+};
+
+/**
+* Determine if 2 points are connected
+*
+* @param {Int} p First point
+* @param {Int} q Second point
+* @return {Boolean}
+*/
+QuickUnion.prototype.isConnected = function (p, q) {
+	return this.root(p) === this.root(q);
+};
+
+/**
+ * Retrive root of a determine point
+ *
+ * @param {Int} point The point to get the root
+ */
+QuickUnion.prototype.root = function (point) {
+	while (point !== this.map[point]) {
+		point = this.map[point];
+	}
+
+	return point;
+};
+
+module.exports = QuickUnion;
---
+++ test/UnionFind/strategy/quick_union_test.js
@@ -0,0 +1,26 @@
+var QuickUnion = require('../../../src/UnionFind/strategy/quick_union');
+var assert = require('assert');
+
+
+describe('Quick Find', function () {
+
+	it('should find connected points', function () {
+		var pixels = new QuickUnion(10);
+		assert.equal(pixels.isConnected(4, 4), true);
+		assert.equal(pixels.isConnected(2, 1), false);
+		pixels.union(4, 3);
+		pixels.union(3, 8);
+		pixels.union(2, 1);
+		pixels.union(4, 9);
+		pixels.union(5, 6);
+
+		assert.equal(pixels.isConnected(4, 4), true);
+		assert.equal(pixels.isConnected(4, 3), true);
+		assert.equal(pixels.isConnected(3, 4), true);
+		assert.equal(pixels.isConnected(3, 8), true);
+		assert.equal(pixels.isConnected(0, 6), false);
+		assert.equal(pixels.isConnected(1, 9), false);
+	});
+
+
+});
